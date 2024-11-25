2630c4dd23239ec63d956a3954c74465eb02ed7c
0PeterAdel <1peteradel@gmail.com>
2024-11-26 01:12:30 +0200

    Hood design & set up local server with Python

---
+++ www/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $('.head').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+        },
+        out: {
+            effect: "bounceOut",
+        },
+
+    });
+});
