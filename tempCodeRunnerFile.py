0fb2f060caa6b22e63c25290bd94405561293280
Peter Adel <148042680+0PeterAdel@users.noreply.github.com>
2024-11-26 16:56:35 +0200

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-import os
-import eel
-
-eel.init("www")
-
-# open browser
-os.system('start msedge.exe --app="http://localhost:8000/index.html" ')
-
-eel.start('index.html', mode=None, host='localhost', block=True)
