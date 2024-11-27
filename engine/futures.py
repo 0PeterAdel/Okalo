2011ad2177372036ff3afd110b5905be8a338113
0PeterAdel <1peteradel@gmail.com>
2024-11-27 14:54:07 +0200

    chat-bar && Sound effect

---
+++ engine/futures.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+# Start Engine Sound fun
+def playAssistantSound():
+    music_dir = "www\\assets\\audio\\start-engine.mp3"
+    playsound(music_dir)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 import eel
+from engine.futures import *
 
 eel.init("www")
+
+# Start engine sound fun
+playAssistantSound()
 
 # open browser
 os.system('start msedge.exe --app="http://localhost:8000/index.html" ')
